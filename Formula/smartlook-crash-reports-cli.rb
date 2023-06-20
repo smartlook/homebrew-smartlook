ba328f64f2df2b3c65876c9dd10303b50fe7adef
Jan Zaloudek <honzicekz@gmail.com>
2023-06-20 17:04:07 +0200

    smartlook-crash-reports-cli updated to 2.1.6

--- Formula/smartlook-crash-reports-cli.rb
+++ Formula/smartlook-crash-reports-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class SmartlookCrashReportsCli < Formula
   desc "CLI tool for uploading crash mapping files to the Smartlook"
   homepage "https://www.npmjs.com/package/@smartlook/smartlook-crash-reports-cli"
-  url "https://registry.npmjs.org/@smartlook/smartlook-crash-reports-cli/-/smartlook-crash-reports-cli-2.1.4.tgz"
-  sha256 "feba2d06d1549aa43a9d8e5c006abd7eb7691a7fb09515ab2cac69420040dfa0"
+  url "https://registry.npmjs.org/@smartlook/smartlook-crash-reports-cli/-/smartlook-crash-reports-cli-2.1.6.tgz"
+  sha256 "861e209cde92799e04a83a236cdbafc6f9328c10b94cc173cd7f0d4d5e41d6dd"
   license "MIT"
 
   depends_on "node"
@@ -16,6 +16,6 @@ class SmartlookCrashReportsCli < Formula
 
   test do
     output = shell_output("#{bin}/smartlook-crash-reports --version")
-    assert_match(/ 2\.1\.4/, output)
+    assert_match(/ 2\.1\.6/, output)
   end
 end
